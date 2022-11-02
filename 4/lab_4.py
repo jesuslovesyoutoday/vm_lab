3f43a18f3496c14929fb3f1fe36b33bef1ad9479
scandiy <jesuslovesyoutoday@yandex.ru>
2022-11-03 01:11:23 +0300

    fixed spline coeffs

--- 4/lab_4.py
+++ 4/lab_4.py
@@ -1,7 +1,21 @@
 from matplotlib import pyplot as plt
+from sympy import simplify
+from sympy import Symbol
 import pandas as pd
 import numpy as np
 import math
+
+def solve(X, diff):
+    x = Symbol('x')
+    print(simplify(diff[0][0] + (x-X[0])*diff[1][0] + (x-X[0])*(x-X[1])*diff[2][0] + (x-X[0])*(x-X[1])*(x-X[2])*diff[3][0] + (x-X[0])*(x-X[1])*(x-X[2])*(x-X[3])*diff[4][0] + (x-X[0])*(x-X[1])*(x-X[2])*(x-X[3])*(x-X[4])*diff[5][0] + (x-X[0])*(x-X[1])*(x-X[2])*(x-X[3])*(x-X[4])*(x-X[5])*diff[6][0]), '\n')
+
+a_0 = 0.983 
+a_1 = -4.142
+a_2 = 7.193
+a_3 = -6.658
+a_4 = 3.519
+a_5 = -1.036
+a_6 = 0.142
 
 #------------------Разделенные разности-----------------#
 def dev_dif(x, y):
@@ -18,6 +32,10 @@ def dev_dif(x, y):
             f.append((y_[j] - y_[j+1])/(x[j] - x[j+1+p]))
         diff.append(f)
     return diff
+#----------------Производная интерполянта---------------#
+
+def derivate(x0):
+    return 6*x0**5*a_6 + 5*x0**4*a_5 + 4*x0**3*a_4 + 3*x0**2*a_3 + 2*x0*a_2 + a_1
 
 #-----------------------Сплайн--------------------------#
 
@@ -58,6 +76,14 @@ def interpolate(x0, diff):
     a2 = xx[ind]/2
     a3 = d[ind]/6
 
+    xx = [i/2 for i in xx]
+    d = [i/6 for i in d]
+
+    print('a0', y)
+    print('a1', b)
+    print('a2', xx)
+    print('a3', d)
+
     print('a0 = ', a0)
     print('a1 = ', a1)
     print('a2 = ', a2)
@@ -68,17 +94,57 @@ def interpolate(x0, diff):
 
     return res
 
+#-----------------------------------------------------#
+
+def coeff(x, f):
+    a0 = []
+    a1 = []
+    a2 = []
+    a3 = []
+    for i in range(len(x)-1):
+        xi  = x[i]
+        xi1 = x[i+1]
+        fi  = f[i]
+        fi1 = f[i+1]
+        pi  = derivate(xi)
+        pi1 = derivate(xi1) 
+        
+        a0.append((-pi1 * xi**2 *xi1 * (xi1 - xi) + fi1 * xi**2 * (3 * xi1 - xi) + fi * xi1**2 * (xi1 - 3 * xi) - pi * xi * xi1**2 * (xi1 - xi))/(xi1 - xi)**3)
+        a1.append((pi1 * xi * (2 * xi1 + xi) * (xi1 - xi) - 6 * (fi1 - fi) * xi * xi1 + pi * xi1 * (xi1 + 2 * xi) * (xi1 - xi))/(xi1-xi)**3)
+        a2.append((-pi1 * (xi1 - xi) * (xi1 + 2 * xi) + 3 * (fi1 - fi) * (xi1 + xi) - pi * (xi1 - xi) * (xi + 2 * xi1))/ (xi1 - xi)**3)
+        a3.append((pi1 * (xi1 - xi) - 2 * (fi1 - fi) + pi * (xi1 - xi))/ (xi1 - xi)**3)        
+
+    print('a0: ', a0)
+    print('a1: ', a1)
+    print('a2: ', a2)
+    print('a3: ', a3, '\n')
+    return[a0, a1, a2, a3]               
+
+#------------------------------------------------------#
+
+def res(x0, x, a0, a1, a2, a3):
+    ind = (np.abs(x - x0)).argmin()
+    dx = x0 - x[ind]
+    return x0**3 * a3[ind] + x0**2 * a2[ind] + x0 * a1[ind] + a0[ind]
+
+#------------------------------------------------------#
+
 data = pd.read_csv('data.txt', sep = ' ')
 x = (data.loc[:, "x"].to_numpy())
 y = (data.loc[:, "y"].to_numpy())
 
-step = x[1] - x[0]
+diff = dev_dif(x, y)
+solve(x, diff)
+c = coeff(x, diff[0])
 
 print("Введите точку x0:\n")
+
 x0 = float(input())
 
-diff = dev_dif(x, y)
-interpolate(x0, diff)
+print(res(x0, x, c[0], c[1], c[2], c[3]))
+
+
+
 
 '''x_ = np.linspace(1, 2.7, 20)
 y_ = []
